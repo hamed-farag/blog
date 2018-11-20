23ec6abaf87fec200b207669a2a06601cb2adfe4
Hamed Farag <hamed.farag.2009@gmail.com>
2018-11-20 18:58:19 +0200

    Enhance Package.json & gatsbyConfig files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
-    description: 'A starter blog demonstrating what Gatsby can do.',
-    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
+    title: `Hamed's Blog`,
+    author: 'Hamed Farag',
+    description: 'Personal Blog Stuffed with Code',
+    siteUrl: '',
   },
-  pathPrefix: '/gatsby-starter-blog',
+  pathPrefix: '/hamed-farag-blog',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
