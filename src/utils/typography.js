6fae313a5aef625e2bef72693e3177db28db1dc4
Hamed Farag <hamed.farag.2009@gmail.com>
2018-11-20 19:24:07 +0200

    Replace WordPress with github typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,21 +1,13 @@
-import Typography from 'typography'
-import Wordpress2016 from 'typography-theme-wordpress-2016'
+import Typography from 'typography';
+import githubTheme from 'typography-theme-github';
 
-Wordpress2016.overrideThemeStyles = () => ({
-  'a.gatsby-resp-image-link': {
-    boxShadow: 'none',
-  },
-})
-
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(githubTheme);
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
-  typography.injectStyles()
+  typography.injectStyles();
 }
 
-export default typography
-export const rhythm = typography.rhythm
-export const scale = typography.scale
+export default typography;
+export const rhythm = typography.rhythm;
+export const scale = typography.scale;
